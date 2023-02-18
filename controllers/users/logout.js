53e373d7d8f49dd1ddb51bc287194b1f1aabb2d0
Nikita Sokolov <nsokolov0309@gmail.com>
2023-02-18 20:36:17 +0200

    logout, token: null into bd

--- controllers/users/logout.js
+++ controllers/users/logout.js
@@ -1,10 +1,10 @@
-// const { Users } = require("../../models/modelUser");
+const { Users } = require("../../models/modelUser");
 
 const logout = async (req, res, next) => {
   try {
     const user = req.user;
 
-    console.log(user);
+    await Users.findByIdAndUpdate({ _id: user._id }, { token: null });
 
     res.status(201).json({ message: "You are log out" });
   } catch (error) {
