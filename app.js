4f0f6077d244e810ac892358310bb25505d0286f
belyy10 <maximgyda10@icloud.com>
2023-02-19 17:00:49 +0300

    small fix

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const cors = require("cors");
 
 const authRouter = require("./routes/api/auth-routes");
-const transactionRouter = require('./routes/api/transactions')
+const transactionRouter = require("./routes/api/transactions");
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", authRouter);
-app.use('/api/transaction', transactionRouter)
+app.use("/api/transaction", transactionRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
