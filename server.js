2821b9b17e92bc86efc1eeebcf2db0095bec68e7
Valerii-Postrybailo <postribailo.valeri@gmail.com>
2023-02-21 16:15:57 +0200

    first open version of swagger-ui

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ async function main() {
     console.log("Database connection successful");
     app.listen(3000, () => {
       console.log("Server running. Use our API on port: 3000");
+      console.log("Docs available at http://localhost:3000/api-docs/")
     });
   } catch (error) {
     console.error("Main failed:", error.message);
