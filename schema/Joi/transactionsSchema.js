7a897bf551f28bf7a49864cfe236516faa09807e
Tetiana Zaitseva <tatyana.lytvyniuk@gmail.com>
2023-02-22 19:06:20 +0100

    add day to model and Joi

--- schema/Joi/transactionsSchema.js
+++ schema/Joi/transactionsSchema.js
@@ -5,6 +5,7 @@ const addTransactionsIncomesSchema = Joi.object({
   description: Joi.string().required(),
   sum: Joi.number().required(),
   type: Joi.string().valid("incomes").required(),
+  day: Joi.number().integer().max(31).min(1).required(),
   month: Joi.number().integer().max(12).min(1).required(),
   year: Joi.number().integer().max(2023).min(2020).required(),
 });
@@ -14,6 +15,7 @@ const addTransactionsExpensesSchema = Joi.object({
   description: Joi.string().required(),
   sum: Joi.number().required(),
   type: Joi.string().valid("expenses").required(),
+  day: Joi.number().integer().max(31).min(1).required(),
   month: Joi.number().integer().max(12).min(1).required(),
   year: Joi.number().integer().max(2023).min(2020).required(),
 });
