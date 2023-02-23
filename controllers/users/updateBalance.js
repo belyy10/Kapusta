cf88cf951940d5e195e55cfb6f44a51809899cb3
Valerii-Postrybailo <postribailo.valeri@gmail.com>
2023-02-23 13:47:28 +0200

    completed swagger and some small checks

--- controllers/users/updateBalance.js
+++ controllers/users/updateBalance.js
@@ -1,26 +1,34 @@
 const { Users } = require("../../models/modelUser");
-const { HttpError } = require("../../helpers/index");
+const { BadRequest, Unauthorized } = require("http-errors");
 
 const updateBalance = async (req, res, next) => {
   try {
     const { _id } = req.user._id;
     const { balance } = req.body;
-    const updateUser = await Users.findByIdAndUpdate(
-      _id,
-      { balance },
-      { new: true }
-    );
-    if (!updateUser) {
-      throw new HttpError();
+
+    if (balance) {
+      const updateUser = await Users.findByIdAndUpdate(
+        _id,
+        { balance },
+        { new: true }
+      );
+      if (!updateUser) {
+        return Unauthorized();
+      }
+  
+      res.json({
+        status: "success",
+        code: 200,
+        user: {
+          email: updateUser.email,
+          balance: updateUser.balance,
+        },
+      });
+    } else{ 
+      return next(BadRequest("Wrong input or missing required field"));
     }
-    res.json({
-      status: "success",
-      code: 200,
-      user: {
-        email: updateUser.email,
-        balance: updateUser.balance,
-      },
-    });
+
+    
   } catch (error) {
     next(error);
   }
