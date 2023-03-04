9d5ec5403798369597759e604420c26c54377a76
Tetiana Zaitseva <tatyana.lytvyniuk@gmail.com>
2023-03-04 12:05:08 +0100

    separate schema from model, remove validation Joi, fix data in model

--- models/transaction.js
+++ models/transaction.js
@@ -1,47 +1,9 @@
 const mongoose = require("mongoose");
+const {
+  transactionSchema,
+} = require("../schema/mongooseSchema/transactionSchema");
 
-const schema = mongoose.Schema(
-  {
-    date: {
-      type: Date,
-      default: Date.now,
-    },
-    category: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    sum: {
-      type: Number,
-      required: true,
-    },
-    type: {
-      type: String,
-      required: true,
-    },
-    owner: {
-      type: mongoose.SchemaTypes.ObjectId,
-      ref: "user",
-    },
-    month: {
-      type: Number,
-      required: true,
-    },
-    year: {
-      type: Number,
-      required: true,
-    }
-  },
-  {
-    versionKey: false,
-    timestamps: true,
-    collection: "Transactions",
-  }
-);
-const Transaction = mongoose.model("transaction", schema);
+const Transaction = mongoose.model("transaction", transactionSchema);
 
 module.exports = {
   Transaction,
