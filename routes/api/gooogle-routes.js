1836aec96452c7a0adec8c8193780fdae99cab11
belyy10 <maximgyda10@icloud.com>
2023-03-05 20:33:13 +0200

    fix

--- routes/api/gooogle-routes.js
+++ routes/api/gooogle-routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { users: ctrl } = require("../../controllers");
 
 router.get("/google", ctrl.googleAuth);
-router.get("/google/callback", ctrl.googleRedirect);
+router.get("/google-redirect", ctrl.googleRedirect);
 
 module.exports = router;
