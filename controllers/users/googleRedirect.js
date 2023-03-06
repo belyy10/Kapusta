d67e03ddbac9a5672b5c05f2bcdaada01a0fde7e
belyy10 <maximgyda10@icloud.com>
2023-03-06 19:34:46 +0200

    fix

--- controllers/users/googleRedirect.js
+++ controllers/users/googleRedirect.js
@@ -59,14 +59,19 @@ async function googleRedirect(req, res, next) {
     const accessToken = jwt.sign({ id: user.id }, JWT_CODE, {
       expiresIn: "1d",
     });
+    const refreshToken = jwt.sign({ id: user.id }, JWT_CODE, {
+      expiresIn: "30d",
+    });
+
     await Users.findByIdAndUpdate(
       { _id: user._id },
       { accessToken: accessToken },
+      { refreshToken: refreshToken },
       { new: true }
     );
 
     return res.redirect(
-      `${FRONTEND_URL}?email=${user.email}&accessToken=${accessToken}`
+      `${FRONTEND_URL}?email=${user.email}&accessToken=${accessToken}&refreshToken=${refreshToken}`
     );
   } catch (error) {
     next(error);
