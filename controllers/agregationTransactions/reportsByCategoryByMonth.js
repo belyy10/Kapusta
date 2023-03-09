8041bf3a5d6671f4d62be3d76dc6f01b40fde4c6
Natali Poliakova <99250547+Nataliphoenix@users.noreply.github.com>
2023-03-09 18:42:12 +0200

    fix

--- controllers/agregationTransactions/reportsByCategoryByMonth.js
+++ controllers/agregationTransactions/reportsByCategoryByMonth.js
@@ -6,11 +6,12 @@ const { nanoid } = require("nanoid");
 
 async function reportsByCategoryByMonth(req, res, next) {
   const { _id } = req.user;
-  const { type } = req.query;
+  const { type, date } = req.query;
 
-  console.log("req.params", req.query);
+  const dateYear = Number(date.slice(0, 4));
+  const dateMon = Number(date.slice(5, 7));
 
-  if (!type) {
+  if (!type || !date) {
     return next(BadRequest("Bad Request"));
   }
 
@@ -25,49 +26,29 @@ async function reportsByCategoryByMonth(req, res, next) {
         $match: {
           owner: _id,
           type: type,
-        },
-      },
-      {
-        $group: {
-          _id: {
-            $dateToString: { format: "%Y-%m", date: "$date" },
+          $expr: { $and:
+            [{ $eq: [dateMon, { $month: "$date" }]},
+              { $eq: [dateYear, { $year: "$date" }] }],
           },
         },
-      },
+  },
+
       {
         $group: {
           _id: "$category",
+          sum: { $sum: "$sum" },
         },
       },
       {
         $project: {
+          _id: 0,
           id: nanoid(),
-          name: "$category",
-          sum: { $sum: "$sum" },
+          name: "$_id",
+          sum: "$sum",
         },
       },
-      //     {
-      //       $project: {
-      //         _id: _id.date,
-      //           expenses: { 'Products' : {$sum: { $cond: [{ $eq: ['$category', 'Products']}, '$sum',0 ]}},
-      //                       'Alcohol' : {$sum: { $cond: [{ $eq: ['$category', 'Alcohol']}, '$sum',0 ]}},
-      //                       'Entertainment' : {$sum: { $cond: [{ $eq: ['$category', 'Entertainment']}, '$sum',0 ]}},
-      //                       'Health' : {$sum: { $cond: [{ $eq: ['$category', 'Health']}, '$sum',0 ]}},
-      //                       'Transport' : {$sum: { $cond: [{ $eq: ['$category', 'Transport']}, '$sum',0 ]}},
-      //                       'Housing' : {$sum: { $cond: [{ $eq: ['$category', 'Housing']}, '$sum',0 ]}},
-      //                       'Technique' : {$sum: { $cond: [{ $eq: ['$category', 'Technique']}, '$sum',0 ]}},
-      //                       'Communal, communication' : {$sum: { $cond: [{ $eq: ['$category', 'Communal, communication']}, '$sum',0 ]}},
-      //                       'Sports, hobbies' : {$sum: { $cond: [{ $eq: ['$category', 'Sports, hobbies']}, '$sum',0 ]}},
-      //                       'Education' : {$sum: { $cond: [{ $eq: ['$category', 'Education']}, '$sum',0 ]}},
-      //                       'Other' : {$sum: { $cond: [{ $eq: ['$category', 'Other']}, '$sum',0 ]}},
-      //          },
-      //           incomes: {  'Salary': {$sum: { $cond: [{ $eq: ['$category', 'Salary'] }, '$sum', 0 ]}},
-      //                       'AddIncome': {$sum: { $cond: [{ $eq: ['$category', 'Add income'] }, '$sum', 0 ]}},
-      //         },
-      //   },
-      // },
       {
-        $sort: { sum: 1 },
+        $sort: { sum: -1 },
       },
     ]);
     return res.status(200).json(result);
